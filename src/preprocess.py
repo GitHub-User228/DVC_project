0c5eda4425b707d5aa811c64b0944c4a643c47f6
GitHub-User228 <73312904+GitHub-User228@users.noreply.github.com>
2023-03-22 11:53:47 +0300

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,17 +5,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import yaml
 
+print('================================')
+
 def split_data(data, test_size, seed):
-  train, val = train_test_split(data, test_size=test_size, seed=seed)
+  train, val = train_test_split(data, test_size=test_size, random_state=seed)
   return train, val
 
 def preprocess_data(train, val):
     scaler = MinMaxScaler()
-    train = scaler.fit_transform(train)
-    val = scaler.transform(val)
+    train[:] = scaler.fit_transform(train)
+    val[:] = scaler.transform(val)
     return train, val
 
-params = yaml.safe_load(open("params.yaml"))["prepare"]
+params = yaml.safe_load(open("params.yaml"))["preprocess_data"] ##############
   
 SEED = params["seed"]
 TEST_SIZE = params["split"]
@@ -24,16 +26,19 @@ pathname = os.path.dirname(sys.argv[0])
 path = os.path.abspath(pathname)
 
 input = sys.argv[1]
-prepared_data_path = os.path.join(path, 'data', 'prepared')
+prepared_data_path = sys.argv[2]
+#prepared_data_path = os.path.join(path, 'data', 'prepared')
 if not os.path.exists(prepared_data_path):
     os.makedirs(prepared_data_path)
 
     
-data = pd.read_csv(input)
+data = pd.read_csv(input, index_col=0)
 
 train, val = split_data(data, TEST_SIZE, SEED)
 
 train, val = preprocess_data(train, val)
+
+print(train.shape, val.shape)
 
 train.to_csv(os.path.join(prepared_data_path, 'train.csv'))
 val.to_csv(os.path.join(prepared_data_path, 'val.csv'))    
